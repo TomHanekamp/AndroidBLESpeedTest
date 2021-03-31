453246bebc18eb50de424d67b5a11a71b9fce698
Tom Hanekamp <tom.hanekamp@luminis.eu>
2021-03-31 12:42:20 +0200

    Added a BleManager implementation and BleService.

---
+++ app/src/main/java/nl/tomhanekamp/blespeedtest/ble/BleManagerImpl.kt
@@ -0,0 +1,69 @@
+package nl.tomhanekamp.blespeedtest.ble
+
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCharacteristic
+import android.content.Context
+import no.nordicsemi.android.ble.BleManager
+import no.nordicsemi.android.ble.BleManagerCallbacks
+import no.nordicsemi.android.ble.callback.*
+import timber.log.Timber
+import java.util.*
+
+class BleManagerImpl(context: Context): BleManager<BleManagerCallbacks>(context) {
+
+    companion object{
+        const val SERVICE_UUID = "5fbfb456-a20b-478f-889c-b3fa3329cd7d"
+        const val WRITE_CHARACTERISTIC_UUID = "6bebf1b3-e6fd-47a4-8ed9-b36365c3e654"
+        const val NOTIFY_CHARACTERISTIC_UUID = "bc30689f-9105-4789-b2e9-8865637f50a0"
+    }
+
+    interface RequestCallback: FailCallback, InvalidRequestCallback, DataSentCallback
+
+    var writeCharacteristic: BluetoothGattCharacteristic? = null
+    var readCharacteristic: BluetoothGattCharacteristic? = null
+
+    fun setMTU(value: Int, callback: MtuCallback) {
+        requestMtu(value)
+            .with(callback)
+            .enqueue()
+    }
+
+    fun initNotifications(notifyCallback: DataReceivedCallback) {
+        Timber.i("Registering notification on read characteristic")
+        setNotificationCallback(readCharacteristic).with(notifyCallback)
+        enableNotifications(readCharacteristic).enqueue()
+    }
+
+    fun send(data: ByteArray, callback: RequestCallback) {
+        writeCharacteristic?.let {
+            it.writeType = BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
+            writeCharacteristic(it, data)
+                .fail(callback)
+                .invalid(callback)
+                .with(callback)
+                .enqueue()
+        }
+    }
+
+    override fun getGattCallback(): BleManagerGattCallback {
+        return gattCallback
+    }
+
+    private val gattCallback: BleManagerGattCallback = object : BleManagerGattCallback() {
+        override fun isRequiredServiceSupported(gatt: BluetoothGatt): Boolean {
+            val service = gatt.getService(UUID.fromString(SERVICE_UUID))
+            return if (service == null) {
+                false
+            } else {
+                writeCharacteristic = service?.getCharacteristic(UUID.fromString(WRITE_CHARACTERISTIC_UUID))
+                readCharacteristic = service?.getCharacteristic(UUID.fromString(NOTIFY_CHARACTERISTIC_UUID))
+                writeCharacteristic != null && readCharacteristic != null
+            }
+        }
+
+        override fun onDeviceDisconnected() {
+            Timber.i("onDeviceDisconnected")
+        }
+    }
+}
+
---
+++ app/src/main/java/nl/tomhanekamp/blespeedtest/ble/BleService.kt
@@ -0,0 +1,251 @@
+package nl.tomhanekamp.blespeedtest.ble
+
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.os.ParcelUuid
+import no.nordicsemi.android.ble.BleManagerCallbacks
+import no.nordicsemi.android.ble.callback.DataReceivedCallback
+import no.nordicsemi.android.ble.callback.MtuCallback
+import no.nordicsemi.android.ble.data.Data
+import no.nordicsemi.android.support.v18.scanner.*
+import timber.log.Timber
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.util.*
+import kotlin.math.ceil
+
+interface BleServiceCallbacks {
+    fun testAborted(reason: String)
+    fun mtuDetermined(mtuValue: Int)
+    fun speedDetermined(bytesPerSecond: Int)
+}
+
+@ExperimentalStdlibApi
+class BleService(private val context: Context, private val transferFileId: Int, private val callbacks: BleServiceCallbacks) {
+
+    companion object {
+        private const val SCAN_TIMEOUT: Long = 30000
+        private const val MTU_REQUEST_SIZE: Int = 512
+    }
+
+    private val bleManager: BleManagerImpl = BleManagerImpl(context)
+    private val scanTimer: Timer = Timer()
+
+    private val bleScanLock: Object = Object()
+    private val bleConnectionLock: Object = Object()
+
+    private var bleScanInProgress: Boolean = false
+    private var bleConnectionInProgress: Boolean = false
+    private var bleConnected: Boolean = false
+
+    private var dataToSend: MutableList<ByteArray> = mutableListOf()
+    private var totalBytesToSend: Int? = null
+    private var startTime: Date? = null
+
+    fun startBleTest() {
+        synchronized(bleScanLock) {
+            if (bleScanInProgress) {
+                Timber.w("BLE scan not started, a scan is already in progress.")
+            } else {
+                val scanSettings = ScanSettings.Builder()
+                    .setLegacy(false)
+                    .setScanMode(ScanSettings.SCAN_MODE_BALANCED)
+                    .setMatchMode(ScanSettings.MATCH_MODE_STICKY)
+                    .setUseHardwareBatchingIfSupported(true)
+                    .build()
+
+                scanTimer.schedule(object : TimerTask() {
+                    override fun run() {
+                        BluetoothLeScannerCompat.getScanner().stopScan(scanCallback)
+                        callbacks.testAborted("No suitable server found.")
+                    }
+                }, SCAN_TIMEOUT)
+
+                bleScanInProgress = true
+
+                val scanFilters = listOf(
+                    ScanFilter.Builder()
+                        .setServiceUuid(ParcelUuid(UUID.fromString(BleManagerImpl.SERVICE_UUID)))
+                        .build()
+                )
+                val scanner = BluetoothLeScannerCompat.getScanner()
+                scanner.startScan(scanFilters, scanSettings, scanCallback)
+            }
+        }
+    }
+
+    fun abortBleTest() {
+        synchronized(bleScanLock) {
+            if (bleScanInProgress) {
+                bleScanInProgress = false
+                BluetoothLeScannerCompat.getScanner().stopScan(scanCallback)
+                callbacks.testAborted("BLE test aborted.")
+            }
+        }
+
+        closeBleConnection("BLE test aborted")
+    }
+
+    private val scanCallback: ScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            synchronized(bleScanLock) {
+                bleScanInProgress = false
+                BluetoothLeScannerCompat.getScanner().stopScan(this)
+                openBleConnection(result.device)
+            }
+        }
+
+        override fun onBatchScanResults(results: List<ScanResult>) {
+            synchronized(bleScanLock) {
+                bleScanInProgress = false
+                results.firstOrNull()?.let {
+                    BluetoothLeScannerCompat.getScanner().stopScan(this)
+                    openBleConnection(it.device)
+                }
+            }
+        }
+
+        override fun onScanFailed(errorCode: Int) {
+            synchronized(bleScanLock) {
+                bleScanInProgress = false
+                BluetoothLeScannerCompat.getScanner().stopScan(this)
+                callbacks.testAborted("BLE scan has failed.")
+            }
+        }
+    }
+
+    private fun openBleConnection(device: BluetoothDevice) {
+        synchronized(bleConnectionLock) {
+            when {
+                bleConnectionInProgress -> {
+                    Timber.w("BLE connection not started, a connection is already in progress.")
+                }
+                bleConnected -> {
+                    Timber.w("BLE connection not started, a connection is already open.")
+                }
+                else -> {
+                    bleConnectionInProgress = true
+                    bleManager.setGattCallbacks(bleManagerCallbacks)
+                    bleManager.connect(device)
+                        .retry(3, 500)
+                        .useAutoConnect(false)
+                        .enqueue()
+                }
+            }
+        }
+    }
+
+    private fun closeBleConnection(reason: String) {
+        synchronized(bleConnectionLock) {
+            if (bleConnectionInProgress || bleConnected) {
+                bleConnectionInProgress = false
+                bleConnected = false
+                bleManager.disconnect()
+                    .done {
+                        callbacks.testAborted(reason)
+                    }
+                    .enqueue()
+            }
+        }
+    }
+
+    private val bleManagerCallbacks = object : BleManagerCallbacks {
+        override fun onDeviceReady(device: BluetoothDevice) {
+            synchronized(bleConnectionLock) {
+                bleConnectionInProgress = false
+                bleConnected = true
+            }
+            bleManager.setMTU(MTU_REQUEST_SIZE, mtuCallback)
+        }
+
+        override fun onDeviceDisconnected(device: BluetoothDevice) {
+            closeBleConnection("Device has disconnected")
+        }
+
+        override fun onDeviceNotSupported(device: BluetoothDevice) {
+            closeBleConnection("Device is not supported")
+        }
+
+        override fun onLinkLossOccurred(device: BluetoothDevice) {
+            closeBleConnection("Link loss has occurred")
+        }
+
+        override fun onError(device: BluetoothDevice, message: String, errorCode: Int) {
+            closeBleConnection("An error has occurred in the BLE connection: $errorCode")
+        }
+
+        override fun onDeviceDisconnecting(device: BluetoothDevice) {}
+        override fun onDeviceConnected(device: BluetoothDevice) {}
+        override fun onBondingFailed(device: BluetoothDevice) {}
+        override fun onServicesDiscovered(device: BluetoothDevice, optionalServicesFound: Boolean) {}
+        override fun onBondingRequired(device: BluetoothDevice) {}
+        override fun onBonded(device: BluetoothDevice) {}
+        override fun onDeviceConnecting(device: BluetoothDevice) {}
+    }
+
+    private val mtuCallback = MtuCallback { _, mtu ->
+        callbacks.mtuDetermined(mtu)
+
+        bleManager.initNotifications(notifyCallback)
+        readData(mtu)
+        startTime = Date()
+
+        if (dataToSend.isNotEmpty()) {
+            bleManager.send(dataToSend.removeFirst(), requestCallback)
+        }
+    }
+
+    private val notifyCallback = DataReceivedCallback { _, data ->
+        Timber.i("Received $data from BLE device")
+    }
+
+    private val requestCallback = object : BleManagerImpl.RequestCallback {
+        override fun onRequestFailed(device: BluetoothDevice, status: Int) {
+            closeBleConnection("BLE request has failed")
+        }
+
+        override fun onInvalidRequest() {
+            closeBleConnection("BLE request is invalid")
+        }
+
+        override fun onDataSent(device: BluetoothDevice, data: Data) {
+            if (dataToSend.isNotEmpty()) {
+                bleManager.send(dataToSend.removeFirst(), this)
+            } else {
+                startTime?.time?.let { startTimeInMs ->
+                    val durationInSeconds = (Date().time - startTimeInMs) / 1000
+                    totalBytesToSend?.let { bytesSent ->
+                        val bytesPerSecond = bytesSent / durationInSeconds
+                        callbacks.speedDetermined(bytesPerSecond.toInt())
+                    }
+                }
+            }
+        }
+    }
+
+    private fun readData(chunkSize: Int) {
+        val inStream: InputStream = context.resources.openRawResource(transferFileId)
+        val baos = ByteArrayOutputStream()
+        val buff = ByteArray(10240)
+        var i: Int
+        while (inStream.read(buff, 0, buff.size).also { i = it } > 0) {
+            baos.write(buff, 0, i)
+        }
+        dataToSend = divideArray(baos.toByteArray(), chunkSize)
+    }
+
+    private fun divideArray(source: ByteArray, chunkSize: Int): MutableList<ByteArray> {
+        totalBytesToSend = source.size
+        val ret = Array(ceil(source.size / chunkSize.toDouble()).toInt()) { ByteArray(chunkSize) }
+        var start = 0
+        for (i in ret.indices) {
+            if (start + chunkSize > source.size) {
+                System.arraycopy(source, start, ret[i], 0, source.size - start)
+            } else {
+                System.arraycopy(source, start, ret[i], 0, chunkSize)
+            }
+            start += chunkSize
+        }
+        return ret.asList().toMutableList()
+    }
+}
